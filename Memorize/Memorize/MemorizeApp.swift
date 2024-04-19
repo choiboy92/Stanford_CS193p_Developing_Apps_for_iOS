90592eb022ba03e2ff83eaef60f59d953191ad75
Junho Choi <junho_choi@apple.com>
2024-04-19 13:40:33 +0100

    add Memorize

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Junho Choi on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
