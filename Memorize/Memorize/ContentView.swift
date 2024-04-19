2595b06c5c54b03d13bd67139ced827f2cdeca18
choiboy92 <junhochoi13@gmail.com>
2024-04-19 14:47:20 +0100

    lecture 2

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -15,7 +15,7 @@ import SwiftUI
  //    var body: some View {
  //        // some View = can run anything as long as it returns a type view
  //        VStack() {      // VStack is a struct with default parameters
- //      //VStack(content: { -- equivalently passes below as argument
+ //      //VStack(content: { -- equivalently passes below as argument - TupleView
  //            Image(systemName: "globe")  // image struct + named parameter systemName
  //                .imageScale(.large)     // view modifier - a function (method)
  //                .foregroundStyle(.tint)
@@ -44,20 +44,44 @@ import SwiftUI
 
  // Build a custom view element
  struct CardView: View {
-     var isFaceUp: Bool = false
+     // needs to be a var as we want it to change
+     // var isFaceUp: Bool = false     // stored property needs a value when called
+     @State var isFaceUp = false    // STATE - creates a pointer to this variable for temporary state
+     // will get rid of @State when implementing game logic in backend
 
      var body: some View {
+         /*
+         // PASSED A CLOSURE (A FUNCTION AS AN ARGUMENT)
+         ZStack(alignment: .top, content: {
+             RoundedRectangle(cornerRadius: 12)
+         }
+         // THEREFORE CAN REWRITE USING TRAILING CLOSURE SYNTAX:
+         ZStack(alignment: .top) {
+             RoundedRectangle(cornerRadius: 12)
+         }
+         // IF USING DEFAULT ARGUMENTS, CAN REMOVE PARENTHESES:
+         ZStack {
+             RoundedRectangle(cornerRadius: 12)
+         }
+         */
          ZStack() {
+             // can create a constant from types
+             let base = RoundedRectangle(cornerRadius: 12) // use TYPE INFERENCE
+             // equivalent to - let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+             
              if isFaceUp {
-                 RoundedRectangle(cornerRadius: 12)
-                     .foregroundColor(.white)
-                 RoundedRectangle(cornerRadius: 12)
-                     .strokeBorder(lineWidth: 2)
+                 base.foregroundColor(.white)
+                 base.strokeBorder(lineWidth: 2)
                  Text("👻")
                      .font(.largeTitle)
              } else {
-                 RoundedRectangle(cornerRadius: 12)
+                 base
              }
+         }
+         // add a view modifier for when card is tapped - TRAILING CLOSURE SYNTAX
+         .onTapGesture {
+             // print("tapped") // print to console
+             isFaceUp.toggle()   // used @State for variable to enable it to be changed (temporarily)
          }
      }
  }
