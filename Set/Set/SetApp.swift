117a4d4d48dbe4729116ad6579af88b6a0d69865
choiboy92 <junhochoi13@gmail.com>
2024-04-30 13:32:01 +0100

    new Set app

---
+++ Set/Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Junho Choi on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
